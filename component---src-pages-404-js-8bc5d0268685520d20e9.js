bd5d87876fd70d169fdd59a44583f79600797e42
Amanda Yeh <amandayeh123@gmail.com>
2020-10-22 16:30:10 -0700

    Updates

---
+++ component---src-pages-404-js-8bc5d0268685520d20e9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("vrFN");t.default=function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement(l.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8bc5d0268685520d20e9.js.map
