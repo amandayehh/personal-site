3f3c31542622c6106d82ff5a96a490af18781d15
Amanda Yeh <amandayeh123@gmail.com>
2020-10-23 15:19:53 -0700

    added footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,6 +6,9 @@ import "../../css/global.css"
 import "../../css/reset.css"
 import "../../css/type.css"
 
+import me from "../../images/me.png"
+
+import yargle from "../../images/footer/yargle.gif"
 
 class Footer extends Component {
   constructor(props) {
@@ -15,9 +18,24 @@ class Footer extends Component {
 
   render() {
     return (
-    <footer className="footer">
-
-    </footer>
+      <footer className="footer">
+        <div className="footer__links">
+          <div className="footer__links__title">
+            <img className="footer__links__title__img" src={me} alt="Pixelated Amanda's face" />
+            <p className="bold footer__links__title__text">Find me at</p>
+          </div>
+          <p><a href="">www.are.na/amanda</a></p>
+          <p><a href="">github.com/amandayehh</a></p>
+          <p><a href="">aye01@mica.edu</a></p>
+        </div>
+        <div className="footer__note">
+          <div className="footer__note__heart">
+            <p className="footer__note__heart__text">My heart belongs to</p>
+            <img className="footer__note__heart__img" src={yargle} alt="bear running" />
+          </div>
+          <p>2020 © Amanda Yeh</p>
+        </div>
+      </footer>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ import "../css/reset.css"
 import "../css/type.css"
 import "../css/index.css"
 
-
 import voicesAndVoids from "../images/index/voicesandvoids.png"
 import typeMirror from "../images/index/type-mirror.png"
 import kilotype from "../images/index/kilotype.png"
@@ -28,59 +27,67 @@ import poster3 from "../images/index/poster3.png"
 import canvas from "../images/index/canvas.png"
 import ditto from "../images/index/ditto.png"
 import pixel from "../images/index/pixel.png"
+import cat from "../images/cat.gif"
 
 
 const Index = () => (
   <div className="index">
     <SEO title="Amanda Yeh" />
-    <Name />
-    <Sticky />
-    <div className="index__voices">
-      <Window sizeW={"72vw"} sizeH={"390px"} src={voicesAndVoids} title={"Voices and Voids"} alt="Screen shot of Voices and Voids website" isScroll={true} />
-    </div>
+    <div className="index__wrapper">
+      <Name />
+      <Sticky />
+      <div className="index__voices">
+        <Window sizeW={"72vw"} sizeH={"390px"} src={voicesAndVoids} title={"Voices and Voids"} alt="Screen shot of Voices and Voids website" isScroll={true} />
+      </div>
 
-    <div className="index__type-mirror">
-      <Window sizeW={"50vw"} sizeH={"36vw"} src={typeMirror} title={"Type Mirror"} alt="Screen shot of type mirror's website" />
-    </div>
+      <div className="index__type-mirror">
+        <Window sizeW={"50vw"} sizeH={"36vw"} src={typeMirror} title={"Type Mirror"} alt="Screen shot of type mirror's website" />
+      </div>
 
-    <div className="index__kilotype">
-      <Window sizeW={"85vw"} sizeH={"200px"} src={kilotype} title={"Kilotype"} alt="Screen shot of Kilotype's website" isScroll={true} />
-    </div>
+      <div className="index__kilotype">
+        <Window sizeW={"85vw"} sizeH={"200px"} src={kilotype} title={"Kilotype"} alt="Screen shot of Kilotype's website" isScroll={true} />
+      </div>
 
-    <div className="index__sel">
-      <Window sizeW={"120vw"} sizeH={"350px"} src={sel} title={"Explore SEL"} alt="Screen shot of Explore SEL's website" isScroll={true} />
-    </div>
+      <div className="index__sel">
+        <Window sizeW={"120vw"} sizeH={"350px"} src={sel} title={"Explore SEL"} alt="Screen shot of Explore SEL's website" isScroll={true} />
+      </div>
 
-    <div className="index__semaphore">
-      <Window sizeW={"70vw"} sizeH={"57vw"} src={sema} title={"Learn Semaphore"} alt="Screen shot of learnsemaphore.com" />
-    </div>
+      <div className="index__semaphore">
+        <Window sizeW={"70vw"} sizeH={"57vw"} src={sema} title={"Learn Semaphore"} alt="Screen shot of learnsemaphore.com" />
+      </div>
 
-    <div className="index__audio-visualizer">
-      <Window sizeW={"88vw"} sizeH={"58vw"} isVideo={true} src={audioVisualizer} title={"Audio Visualizer"} alt="Video of music animation" />
-    </div>
-    <div className="index__posters">
-      <Poster sizeW={"45vw"} left={"4vw"} top={"7vw"} src={poster1} alt="Poster for photo exhibition" />
-      <Poster sizeW={"40vw"} left={"0vw"} top={"12vw"} src={poster3} alt="Poster for photo a machine learning workshop" />
-      <Poster sizeW={"48vw"} left={"7vw"} top={"0px"} src={poster2} alt="Poster for the all mighty pumpkin spice cult" />
-    </div>
+      <div className="index__audio-visualizer">
+        <Window sizeW={"88vw"} sizeH={"58vw"} isVideo={true} src={audioVisualizer} title={"Audio Visualizer"} alt="Video of music animation" />
+      </div>
+      <div className="index__posters">
+        <Poster sizeW={"45vw"} left={"4vw"} top={"7vw"} src={poster1} alt="Poster for photo exhibition" />
+        <Poster sizeW={"40vw"} left={"0vw"} top={"12vw"} src={poster3} alt="Poster for photo a machine learning workshop" />
+        <Poster sizeW={"48vw"} left={"7vw"} top={"0px"} src={poster2} alt="Poster for the all mighty pumpkin spice cult" />
+      </div>
 
-    <div className="index__canvas">
-      <Book sizeH={"55vw"} src={canvas} title={"Beyond a Blank Canvas"} alt="Book cover for beyond a blank canvas" />
-    </div>
+      <div className="index__canvas">
+        <Book sizeH={"55vw"} src={canvas} title={"Beyond a Blank Canvas"} alt="Book cover for beyond a blank canvas" />
+      </div>
 
-    <div className="index__ditto">
-      <Window sizeW={"60vw"} sizeH={"50vw"} src={ditto} title={"Ditto"} alt="Screeonshot of Ditto's website" isScroll={true} />
-    </div>
+      <div className="index__ditto">
+        <Window sizeW={"60vw"} sizeH={"50vw"} src={ditto} title={"Ditto"} alt="Screeonshot of Ditto's website" isScroll={true} />
+      </div>
 
-    <div className="index__pixel">
-      <Window sizeW={"75vw"} sizeH={"57vw"} src={pixel} title={"Pixelation"} alt="Screeonshot of Pixelation's website" isScroll={true} />
-    </div>
+      <div className="index__pixel">
+        <Window sizeW={"75vw"} sizeH={"57vw"} src={pixel} title={"Pixelation"} alt="Screeonshot of Pixelation's website" isScroll={true} />
+      </div>
 
+    </div>
     <div className="index__colophon">
       <Colophon />
     </div>
-    <div className="index__footer">
-      <Footer />
+    <div className="index__bottom">
+      <div className="index__cat" >
+        <img className="index__cat__img" src={cat} alt="vibing cat" />
+      </div>
+      <div className="index__footer">
+        <Footer />
+      </div>
     </div>
 
 
